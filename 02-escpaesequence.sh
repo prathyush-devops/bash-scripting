cf66b9864651ce05a540064548010f54eff2cce6
Prathyush <godalaprathyushreddy@gmail.com>
2023-08-10 19:18:31 +0530

    updating

---
+++ 02-escpaesequence.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# shebang notation : This tells the script with interpretor this script has to be executed.
+# There can only be one interpretor per script and that should always be the first time of the script.
+
+echo welcome to Bash Scripting
+
+# This is how we can execute the scripts
+# bash scriptname.sh or sh scriptnsme.sh or ./scriptname.sh
+
+# \n : This indicates a new line
+# \t : This gives a tab space
+# Prathyusn\nDevops
+# Prathyush
+# Devops
+
+echo line1
+echo line2
+echo line3
+
+echo -e "line4\nline5"
+# whenever you're using special characters , always enclose them in DOUBLE QUOTES
+# " " : Double Quotes
+# ' ' : Single Quotes
+
+echo -e "Cloud\tDevOps\tare\tBooming"
+echo -e "We\tAre\tLearning\tCloudDevOps\nThis is my batch55"
