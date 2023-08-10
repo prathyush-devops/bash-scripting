ba0b8d1ea4665d141fbd9121678bc39afe4ef3e4
Prathyush <godalaprathyushreddy@gmail.com>
2023-08-10 19:17:11 +0530

    updating

---
+++ 01-sample.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# shebang notation : This tells the script with interpretor this script has to be executed.
+# There can only be one interpretor per script and that should always be the first time of the script.
+
+echo welcome to Bash Scripting
+
+# This is how we can execute the scripts
+# bash scriptname.sh or sh scriptnsme.sh or ./scriptname.sh
+
+echo line1
+echo line2
+echo line3
