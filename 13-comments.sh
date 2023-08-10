0cef1092cd67409f8ab8d9746b6ffb46434e96f8
Prathyush <godalaprathyushreddy@gmail.com>
2023-08-10 19:23:59 +0530

    updating

---
+++ 13-comments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+<<COMMENT
+echo "Cloud Devops Training"
+echo " Sahell scripting"
+a=100
+b=200
+echo @a
+COMMENT
+echo $b
+
+# This is called as Muli-line comment, whaterver we enclosed between <<COMMENT      COMMENT will be marked as comment
