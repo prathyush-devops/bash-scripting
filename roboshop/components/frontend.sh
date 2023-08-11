48523481c3dc21e44360ed758aa5797339795e86
Prathyush <godalaprathyushreddy@gmail.com>
2023-08-11 17:58:47 +0530

    updating

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -19,6 +19,18 @@ else
     echo -e "\e[31m failure \e[0m"
 fi
 
+echo -n "Starting Nginx"
+systemctl enable nginx
+systemctl start nginx
+
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m success \e[0m"
+else 
+    echo -e "\e[31m failure \e[0m"
+fi
+
+
+
 # yum install nginx -y
 # systemctl enable nginx
 # systemctl start nginx
