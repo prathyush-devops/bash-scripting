3b36c3ae52ec2fcc9d415d165f7d7e6da0cab730
Prathyush <godalaprathyushreddy@gmail.com>
2023-08-11 17:39:09 +0530

    updating

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+
+USER_ID =$(id -u)
+
+if [ $USER_ID -ne 0 ] ; then
+    echo -e "\e[32m Script is expected to execcte by the root user or with a sudo privilage \e[0m \n \t Example: sudo bash wrapper.sh frontend"
+    exit 1
+fi
+
 echo "configuring frontend"
 yum install nginx -y
+
 
 # yum install nginx -y
 # systemctl enable nginx
