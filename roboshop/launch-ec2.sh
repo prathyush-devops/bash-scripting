c7c268e6f76ee5296fc541f3499906e08e674b09
Prathyush <godalaprathyushreddy@gmail.com>
2023-08-16 17:59:44 +0530

    GIT Autopush

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -12,4 +12,4 @@ AMI_ID="ami-0c1d144c8fdd8d690"
 INSTANCE_TYPE="t2.micro"
 SG_ID="sg-02431ae2e8f1c8ca8"                               #b55 allow_all security group id
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]'
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
