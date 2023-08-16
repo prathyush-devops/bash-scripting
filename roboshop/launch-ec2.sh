ed99f97ad710d10a2807eb2b61415be29cbba2a6
Prathyush <godalaprathyushreddy@gmail.com>
2023-08-16 17:55:34 +0530

    GIT Autopush

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -1,5 +1,13 @@
 #!/bin/bash 
 
+
+COMPONENT=$1
+if [ -z $1 ] || [ -z $2 ]  ; then 
+    echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
+    echo -e "\e[35m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
+    exit 1
+fi 
+
 AMI_ID="ami-0c1d144c8fdd8d690"
 INSTANCE_TYPE="t2.micro"
 SG_ID="sg-02431ae2e8f1c8ca8"                               #b55 allow_all security group id
