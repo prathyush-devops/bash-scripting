6b5c1ae5bb141e6927da62151af424ef515c019c
Prathyush <godalaprathyushreddy@gmail.com>
2023-08-17 16:23:30 +0530

    GIT Autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -15,7 +15,7 @@ fi
 echo -e "\e[35m Configuring frontend ......! \e[0m \n"
 
 echo -n "Installing frontend :"
-yum install hghkg -y &>> /tmp/frontend.log 
+yum install nginx -y &>> /tmp/frontend.log 
 
 if [ $? -eq 0 ]; then 
         echo -e "\e[32m success \e[0m"
