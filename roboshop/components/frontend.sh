a2617f72a0b67699ebf9a89484d4dfd56887d07d
Prathyush <godalaprathyushreddy@gmail.com>
2023-08-17 16:06:53 +0530

    GIT Autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Validate the user who is running the script is a root user or not.
+
+USER_ID=$(id -u)
+# COMPONENT=frontend
+# LOGFILE="/tmp/${COMPONENT}.log"
+
+
+if [ $USER_ID -ne 0 ] ; then    
+    echo -e "\e[31m Script is expected to executed by the root user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
+    exit 1
+fi 
+
 echo "configuring frontend"
 yum install nginx -y
 
