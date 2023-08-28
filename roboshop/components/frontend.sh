9e8b1f2af06c3f7aa8741d276f76f7d651823a82
Prathyush <godalaprathyushreddy@gmail.com>
2023-08-28 18:09:24 +0530

    Autppush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/bin/bash 
 
 # Validate the user who is running the script is a root user or not.
 
 USER_ID=$(id -u)
 COMPONENT=frontend
 LOGFILE="/tmp/${COMPONENT}.log"
-
 
 if [ $USER_ID -ne 0 ] ; then    
     echo -e "\e[31m Script is expected to executed by the root user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
@@ -17,47 +16,51 @@ stat() {
         echo -e "\e[32m success \e[0m"
     else 
         echo -e "\e[31m failure \e[0m"
-       exit 2
+        exit 2
     fi
 }
 
 echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
 
 echo -n "Installing Nginx :"
-yum install nginx -y &>> ${LOGFILE}
-
+yum install nginx -y     &>>  ${LOGFILE}
 stat $?
 
-echo -n "Starting Nginx:"
-systemctl enable nginx &>> ${LOGFILE} 
-systemctl start nginx &>> ${LOGFILE} 
-
+echo -n "Starting Nginx:" 
+systemctl enable nginx   &>>  ${LOGFILE}
+systemctl start nginx    &>>  ${LOGFILE}
 stat $?
 
 echo -n "Downloading the ${COMPONENT} component:"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
+stat $? 
 
-stat $?
-
-echo -n "Clean up of ${COMPONENT} :"
-cd /usr/share/nginx/html
-rm -rf *     &>> ${LOGFILE}
+echo -n "Clean up of ${COMPONENT} : "
+cd /usr/share/nginx/html    
+rm -rf *     &>>  ${LOGFILE}
 stat $?
 
 echo -n "Extracting ${COMPONENT} :"
-unzip /tmp/frontend.zip   &>> ${LOGFILE}
-stat $?
-
-echo -n "sorting the ${COMPONENT} files :"
-mv ${COMPONENT}-main/* .
-mv static/* .
-rm -rf static README.md     &>> ${LOGFILE}
+unzip /tmp/${COMPONENT}.zip     &>>  ${LOGFILE}
+mv ${COMPONENT}-main/*  .
+mv static/* . 
+rm -rf ${COMPONENT}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
-echo -n "Restarting ${COMPONENT} :"
-systemctl daemon-reload      &>> ${LOGFILE}
-systemctl restart nginx      &>> ${LOGFILE}
+echo -n "Updating the Backend Components in the reverse proxy file:"
+
+for component in catalogue user cart shipping payment ; do 
+    sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+done 
+
+echo -n "Restarting ${COMPONENT}:"
+systemctl daemon-reload     &>>  ${LOGFILE}
+systemctl restart nginx     &>>  ${LOGFILE}
 stat $?
 
-echo -e "\e[35m ${COMPONENT} Installation is completed \e[0m \n"
+echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
+
+
+# I want to ensure, that SCRIPT SHOULD Fail the user who run the scipt is not a root user.
+# rather executing the commands and failing.
--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
-
-#!/bin/bash
+#!/bin/bash 
 
 # Validate the user who is running the script is a root user or not.
 
 USER_ID=$(id -u)
 COMPONENT=mongodb
 LOGFILE="/tmp/${COMPONENT}.log"
-
 
 if [ $USER_ID -ne 0 ] ; then    
     echo -e "\e[31m Script is expected to executed by the root user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
@@ -19,42 +16,43 @@ stat() {
         echo -e "\e[32m success \e[0m"
     else 
         echo -e "\e[31m failure \e[0m"
-       exit 2
+        exit 2
     fi
 }
 
 echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
 
-echo -n "Configuring ${COMPONENT} repo:"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
-stat $?
+echo -n "Configuring ${COMPONENT} repo :"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo 
+stat $? 
 
 echo -n "Installing ${COMPONENT} :"
-yum install -y mongodb-org    &>> ${LOGFILE}
+yum install -y mongodb-org   &>> ${LOGFILE} 
 stat $?
 
 echo -n "Enabling the ${COMPONENT} visibility :"
-sed -ie 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed  -ie 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 stat $?
 
-echo -n "Starting the ${COMPONENT} :"
-systenctl enable mongod       &>> ${LOGFILE}
-systemctl start mongod        &>> ${LOGFILE}
+echo -n "Starting the ${COMPONENT}  :"
+systemctl enable mongod        &>> ${LOGFILE} 
+systemctl start mongod         &>> ${LOGFILE} 
 stat $?
 
-echo -n "Downloading the ${COMPONENT} schema :"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
+echo -n "Downloading the ${COMPONENT} schema: "
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
+stat $? 
 
-echo -n "Extracting the ${COMPONENT} schema :"
-cd /tmp
-unzip -o ${COMPONENT}.zip       &>> ${LOGFILE}
-stat $?
+echo -n "Extracing the ${COMPONENT} Schema:"
+cd /tmp 
+unzip -o ${COMPONENT}.zip &>> ${LOGFILE} 
+stat $? 
 
-echo -n "Injecting ${COMPONENT} schema :"
+
+echo -n "Injecting ${COMPONENT} Schema:"
 cd ${COMPONENT}-main
-mongo < catalogue.js         &>> ${LOGFILE}
-mongo < users.js             &>> ${LOGFILE}
-stat $?
+mongo < catalogue.js    &>>  ${LOGFILE}
+mongo < users.js        &>>  ${LOGFILE}
+stat $? 
 
-echo -e "\e[35m ${COMPONENT} Installation is completed \e[0m \n"
+echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
